39d1b1fc051b111e2567431b47d44c3ab5cdb1dc
ibrahim <ibrahimsn98@gmail.com>
2018-02-09 12:20:42 +0300

    newpage

---
+++ js/scripts-yazgig.js
@@ -0,0 +1,35 @@
+jQuery(function ($) {
+
+    'use strict';
+
+    // --------------------------------------------------------------------
+    // One Page Navigation
+    // --------------------------------------------------------------------
+
+    // --------------------------------------------------------------------
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('a.page-scroll').on('click', function (e) {
+            e.preventDefault();
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop : $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+        });
+    }());
+
+    // --------------------------------------------------------------------
+    // Closes the Responsive Menu on Menu Item Click
+    // --------------------------------------------------------------------
+
+    (function () {
+        $('.navbar-collapse ul li a').on('click', function () {
+            if ($(this).attr('class') != 'dropdown-toggle active' && $(this).attr('class') != 'dropdown-toggle') {
+                $('.navbar-toggle:visible').trigger('click');
+            }
+        });
+    }());
+
+}); // JQuery end
